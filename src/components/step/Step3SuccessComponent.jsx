2d61b768afc71ba86920d1c28144b7e4c6b53866
gitdoolgi <gitdoolgi@gmail.com>
2024-11-10 20:21:05 +0900

    fix: Step3SuccessComponent 배포용 이미지 경로 수정

--- src/components/step/Step3SuccessComponent.jsx
+++ src/components/step/Step3SuccessComponent.jsx
@@ -1,6 +1,6 @@
 import CommonResource from "../../util/CommonResource.jsx";
 import useCustomMove from "../../hooks/useCustomMove.jsx";
-import Save from '../../assets/save.png';
+import Save from 'src/assets/save.png';
 
 export default function Step3SuccessComponent(){
     const {moveToPath}= useCustomMove();
